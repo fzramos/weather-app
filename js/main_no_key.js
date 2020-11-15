3ce7a34950f05b13efafb7c3046ab5f3b8f4ec2b
fzramos <64625115+fzramos@users.noreply.github.com>
2020-11-15 13:26:06 -0600

    Adding current location functionality and cleaning up code -FR

--- js/main_no_key.js
+++ js/main_no_key.js
@@ -8,58 +8,64 @@ STATUS_TO_IMAGE = {
     "Snow": "image/snow.svg",
     "Mist": "image/wind.svg"
 }
-// function cityOrZip() {
-//     user_input = document.getElementById("city").value;
-//     console.log(user_input)
-//     if (Number.isInteger(user_input)) {
-//         getTemp(user_input, 'zip')
-//     }
-//     getTemp(user_input, 'city')
+const myForm = document.getElementById("myForm")
+myForm.addEventListener('keyup', ({key}) => {
+    if (key === "Enter") {
+        getTemp();
+    }
+})
 
-//     // getTemp(user_input, (!Number.isInteger(user_input)))
+/**
+ * NO PARAM
+ * 
+ * Initiates browser request for users current location and passes it to 
+ * getTemp funtion
+ */
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getTemp);
+    }
+}
 
-// }
-
-// const getTemp = (user_in, locType) => {
-//     // if (isCity) {
-//         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${user_in}&appid=${API_KEY}`)
-//         .then(response => response.json())
-//         .then(data => parseShowData(data))
-//         .catch(msg => {
-//             console.log("invalid city")
-//         })
-//     // }
-//     // switch(locType) {
-//     //     case "city":
-//     //         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${user_in}&appid=${API_KEY}`)
-//     //         .then(response => response.json())
-//     //         .then(data => parseShowData(data))
-//     //         .catch(msg => {
-//     //             console.log("invalid city")
-//     //         })
-//     //         break;
-//     //     case "zip":
-//     //         console.log("thats a zip");
-//     //         // zip seems ot work find in the same city call
-//     //         break;
-
-
-            
-//     // }
-// }
-
-const getTemp = () => {
+/**
+ * 
+ * @param position (OPTIONAL)
+ *  
+ * Gets a JSONof the current weather data of a location from Open Weather API using
+ * either the user inputed City/Zip code or the current geolocation of the user.
+ * Object constructed from JSON is then passed to the parseShowData function
+ */
+const getTemp = (position = '') => {
     user_in = document.getElementById("city").value;
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${user_in}&appid=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => parseShowData(data))
-    // .catch(msg => {
-    //     console.log("invalid city")
-    // })
+    if (position == '') {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${user_in}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => parseShowData(data))
+        .catch(msg => {
+            console.log("error in parsing data")
+        })
+    } else {
+        console.log(`${position.coords.latitude}, ${position.coords.longitude}`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(parseShowData)
+        .catch(msg => {
+            console.log("error in parsing data")
+        })
+    
+    }
 }
+
+/**
+ * @param data
+ * 
+ * Parses the current weather object and add the relatent variables to the 
+ * HTML document
+ */
 function parseShowData(data) {
     // getting weather data from json
+    console.log(data)
     const city_name = data.name;
     const temp_max = kToF(data.main.temp_max);
     const temp_min = kToF(data.main.temp_min);
@@ -83,11 +89,14 @@ function parseShowData(data) {
     weather_texts[3].innerHTML = forcast_dets;
     weather_icon.src = STATUS_TO_IMAGE[forcast_title];
     weather_texts[4].innerHTML = `${feels_like} °F`
-
-
-
 }
 
+/**
+ * 
+ * @param kelvin
+ * 
+ * Converts a kelvin temperature value into Farenheit
+ */
 function kToF(kelvin) {
     const rawF = (kelvin - 273.15) * (9/5) + 32;
     return (Math.round(rawF * 100) / 100).toFixed(2)
